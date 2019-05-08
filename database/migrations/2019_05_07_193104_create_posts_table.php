abd2137b6a074b02ec751cbaa835bd087b598056
KillPatrick <g.brusokas@gmail.com>
2019-05-08 06:58:20 +0300

    first commit

---
+++ database/migrations/2019_05_07_193104_create_posts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title', 100);
+            $table->string('subtitle', 100);
+            $table->string('slug', 100)->unique();
+            $table->text('body', 100);
+            $table->boolean('status');
+            $table->integer('posted_by');
+            $table->integer('like')->nullable();
+            $table->integer('dislike')->nullable();
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
